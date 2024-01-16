7163cd2d19ce9119bf72028fc8c60bcf787eb443
sam230823 <156840982+sam230823@users.noreply.github.com>
2024-01-17 00:04:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML ="<h1>peppa pig</h1>";
+  question.innerHTML ="<h1>lubuuuuuu  (happy puppy noises)</h1>";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
